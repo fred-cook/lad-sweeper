15ea34d94b7886176fdad71a0ed1703fae2095bb
fred-cook <duverfred@yahoo.co.uk>
2023-09-12 21:36:01 +0100

    #16 added super init

--- sweeper/gui/cell.py
+++ sweeper/gui/cell.py
@@ -6,5 +6,23 @@ from tkinter.font import Font
 from PIL import Image, ImageTk
 
 class Cell(tk.Button):
-    def __init__(self, master: tk.Tk, coords: tuple[int, int]):
-        pass
+    BUTTON_SIZE = 50 # pixels
+    FONT = Font(family="Arial Rounded MT Bold", size=20)
+    ZERO_IMAGE = tk.PhotoImage(width=0, height=0)
+
+    UNCLICKED = {
+        "text": '',
+        "font": FONT,
+        "image": ZERO_IMAGE,
+        "relief": "raised",
+        "state": tk.ACTIVE,
+        "compound": "center", #  Will show images and text
+    }
+
+    CLICKED = {
+        "relief": "solid",
+        "state": tk.DISABLED,
+        "bd": 1,
+    }
+    def __init__(self, master: tk.Tk, size: int):
+        super().__init__(self, height=size, width=size, **self.UNCLICKED)
